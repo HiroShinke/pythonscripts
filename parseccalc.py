7d2fda18101c25ccb523abbd9bc63712e7c9901d
hirofumi <hiro.shinke@gmail.com>
2017-01-08 15:49:16 +0900

    *** empty log message ***

--- parseccalc.py
+++ parseccalc.py
@@ -1,6 +1,6 @@
 
 
-from parsecp import a,r,para,f,k,l,m,u,o,d,c,opt,sb,sb1,ws,ws1,pS,pR,token,pRef,runParser
+from parsecp import *
 import sys
 import re
 
@@ -12,26 +12,20 @@ def createParser():
     pExpr = None
                 
     def addopObj(t):
-        if t.word == "+":
-            return (lambda n,m: n+m)
-        elif t.word == "-":
-            return (lambda n,m: n-m)
-        else:
-            return None
+        if   t.word == "+": return (lambda n,m: n+m)
+        elif t.word == "-": return (lambda n,m: n-m)
+        else: return None
 
-    pAddop = a( o( tr("""\+"""), tr("""-""") ), addopObj )
+    pAddop = a( o( tr(r"\+"), tr(r"-") ), addopObj )
 
     def mulopObj(t):
-        if t.word == "*":
-            return (lambda n,m: n*m)
-        elif t.word == "/":
-            return (lambda n,m: n/m)
-        else:
-            return None
+        if   t.word == "*": return (lambda n,m: n*m)
+        elif t.word == "/": return (lambda n,m: n/m)
+        else: return None
 
-    pMulop = a( o( tr("""\*"""), tr("""\/""") ), mulopObj )
+    pMulop = a( o( tr(r"\*"), tr(r"\/") ), mulopObj )
         
-    pDigit = a( tr("""\d+"""), lambda t: int(t.word) )
+    pDigit = a( tr(r"\d+"), lambda t: int(t.word) )
     pFactor = o( pDigit,
                  para( ts("("), pRef(lambda : pExpr), ts(")") ) )
     pTerm   = c( pFactor, pMulop )
