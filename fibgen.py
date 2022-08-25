e42e30bd99b275cfd62a0c4eea9e5f47d030c7df
Hirofumi SHINKE <hiro@molloy.local>
2022-08-25 22:33:05 +0900

    take

--- fibgen.py
+++ fibgen.py
@@ -11,10 +11,12 @@ def fib_gen(a,b):
 def fib_iter(a,b):
     yield from itertools.chain([a],fib_iter(b,a+b))
 
+def take(n,iterable):
+    return list(itertools.islice(iterable,n))
     
 def main():
-    print(list(itertools.islice(fib_gen(1,2),0,10)))
-    print(list(itertools.islice(fib_iter(1,2),0,10)))
+    print(take(10,fib_gen(1,2)))
+    print(take(10,fib_iter(1,2)))
     
 if __name__ == "__main__":
     main()
