179bca4991555373d0a48723953794dea11cba0a
Hirofumi SHINKE <hiro@molloy.local>
2022-08-28 11:50:00 +0900

    sieve

--- sieve.py
+++ sieve.py
@@ -1,14 +1,12 @@
 
 
-def fibonacci(a,b):
-    """ Fibonacci generator """
-    while True:
-        yield a
-        a, b = b, a + b
+import itertools
 
-def stream_take(s,n):
-    for i in range(n):
-        yield(next(s))
-        
-print(list(stream_take(fibonacci(0,1), 20)))
+def sieve(n):
+    yield from itertools.chain([n],filter(lambda x: x % n != 0,sieve(n+1)))
+                                              
+def take(itr,n):
+    return list(itertools.islice(itr,n))
+
+print(list(take(sieve(2), 20)))
 
