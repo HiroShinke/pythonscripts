803fab1df16a4fb15a86131f9f2947bbf6096464
Hirofumi SHINKE <hiro@molloy.local>
2022-08-28 11:58:09 +0900

    sieve_gen

--- sieve.py
+++ sieve.py
@@ -3,10 +3,16 @@
 import itertools
 
 def sieve(n):
-    yield from itertools.chain([n],filter(lambda x: x % n != 0,sieve(n+1)))
-                                              
+    yield from itertools.chain([n],
+                               filter(lambda x: x % n != 0,
+                                      sieve(n+1)))
+def sieve_gen(n):
+    yield n
+    yield from filter(lambda x: x % n != 0,
+                      sieve_gen(n+1))
+    
 def take(itr,n):
     return list(itertools.islice(itr,n))
 
 print(list(take(sieve(2), 20)))
-
+print(list(take(sieve_gen(2), 20)))
