08a8335404be24c728b5fa31e40e47385340dc5b
Hirofumi SHINKE <hiro@molloy.local>
2022-09-19 09:55:22 +0900

    use diffobj

--- diff.py
+++ diff.py
@@ -3,21 +3,8 @@
 import sys
 from pathlib import Path
 import difflib
+import diffobj
 import argparse
-
-class DifferPair:
-    def __init__(self,k,o):
-        self.k = k
-        self.o = o
-    def __repr__(self):
-        return f'({self.k}, {self.o})'
-    def __hash__(self):
-        return hash(self.k)
-    def __eq__(self,other):
-        return (
-            self.__class__ == other.__class__ and
-            self.k == other.k
-            )
 
 def do_diff(fp,tp):
     if fp.is_file() and tp.is_file():
@@ -38,23 +25,24 @@ def do_difffile(f,t):
     print(''.join(ret),end='')
 
 def do_diffdir(f,t):
-    seq1 = list( [ DifferPair(n1.name,n1) for n1 in f.iterdir() ])
-    seq2 = list( [ DifferPair(n2.name,n2) for n2 in t.iterdir() ])
 
-    matcher = difflib.SequenceMatcher(a = seq1,b = seq2)
-    # print(seq1)
-    # print(seq2)
-    for tag,i1,i2,j1,j2 in matcher.get_opcodes():
-        # print(f"{tag} a[{i1}:{i2}], b[{j1}:{j2}]")
-        if tag == "equal":
-            for f0,t0 in zip(seq1[i1:i2],seq2[j1:j2]):
-                do_diff(f0.o,t0.o)
-        else:
-            for f0 in seq1[i1:i2]:
-                print(f'{f0.o} only in {f0.o.parent}')
-            for t0 in seq2[j1:j2]:
-                print(f'{t0} only in {t0.o.parent}')
+    seq1 = list( [ n1 for n1 in f.iterdir() ])
+    seq2 = list( [ n2 for n2 in t.iterdir() ])
 
+    def match_func(i,j):
+        do_diff(seq1[i],seq2[j])
+
+    def discard_a(i):
+        print(f'{seq1[i]} only in {seq1[i].parent}')        
+
+    def discard_b(j):
+        print(f'{seq2[j]} only in {seq2[j].parent}')        
+        
+    matcher = diffobj.traverse_sequences(seq1,seq2,
+                                         matchFunc=match_func,
+                                         discardAFunc=discard_a,
+                                         discardBFunc=discard_b,
+                                         keyFunc=lambda x: x.name)
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f"    ,type=str,  action='store')
