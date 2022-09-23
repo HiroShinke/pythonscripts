11c9439a2c78ec00150464c96d98fd309d48bb14
Hirofumi SHINKE <hiro.shinke@gmail.com>
2022-09-23 14:21:31 +0900

    Update tkdiff.py

--- tkdiff.py
+++ tkdiff.py
@@ -7,26 +7,30 @@ from pathlib import Path
 import diffutil
 
 
-def do_diff(fp,tp,of,func=lambda n: n):
+def do_diff(fp,tp,to_sort,of,func=lambda n: n):
     if fp.is_file() and tp.is_file():
-        do_difffile(fp,tp,of,func)
+        do_difffile(fp,tp,to_sort,of,func)
     elif fp.is_dir() and tp.is_dir():
-        do_diffdir(fp,tp,of,func)
+        do_diffdir(fp,tp,to_sort,of,func)
     else:
         print(f"uncomparable {fp} and {tp}",file=of)
     
 
-def do_difffile(f,t,of,func):
+def do_difffile(f,t,to_sort,of,func):
 
 
     seq1 = list(f.open(mode='r'))
     seq2 = list(t.open(mode='r'))
 
+    if to_sort:
+        seq1 = sorted(seq1)
+        seq2 = sorted(seq2)
+
     diffOccurs = False
     lines = []
     
     def match_func(i,j):
-        lines.append("\t".join([" ",f'{i},{j}',f'{seq1[i]}']))        
+        lines.append("\t".join(["",f'{i},{j}',f'{seq1[i]}']))        
     
     def discard_a(i):
         nonlocal diffOccurs
@@ -48,13 +52,13 @@ def do_difffile(f,t,of,func):
         print(f"+++{t}",file=of)
         of.writelines(lines)
 
-def do_diffdir(f,t,of,func):
+def do_diffdir(f,t,to_sort,of,func):
 
     seq1 = list( [ n1 for n1 in f.iterdir() ])
     seq2 = list( [ n2 for n2 in t.iterdir() ])
 
     def match_func(i,j):
-        do_diff(seq1[i],seq2[j],of,func)
+        do_diff(seq1[i],seq2[j],to_sort,of,func)
 
     def discard_a(i):
         print(f'{seq1[i]} only in {seq1[i].parent}',file=of)        
@@ -83,10 +87,12 @@ def main():
     fromString = tk.StringVar()
     toString   = tk.StringVar()
     fileString = tk.StringVar()
+    toSort     = tk.IntVar()
 
     fromString.set(topf)
     toString.set(topt)
     fileString.set(tmpfile)
+    toSort.set(0)
     
     frm = ttk.Frame(root, padding=10)
     frm.grid()
@@ -97,6 +103,8 @@ def main():
     ltv = ttk.Entry(frm, textvariable=toString)
     lfunc=ttk.Label(frm, text="Func:")
     lfuncv = tk.Text(frm, width=80, height=5)
+    ls=ttk.Label(frm, text="Sort:")
+    lsv = ttk.Checkbutton(frm, text="do", variable=toSort)
     lfi=ttk.Label(frm, text="File:")
     lfiv = ttk.Entry(frm, width=80, textvariable=fileString)
     
@@ -104,10 +112,12 @@ def main():
     lfv.grid(column=1, row=0, columnspan=3, sticky="nsew")
     lt.grid(column=0, row=1)
     ltv.grid(column=1, row=1, columnspan=3, sticky="nsew")
-    lfunc.grid(column=0,row=2)
-    lfuncv.grid(column=1,row=2, columnspan=3, sticky="nsew")
-    lfi.grid(column=0,row=3)
-    lfiv.grid(column=1,row=3, columnspan=3, sticky="nsew")
+    ls.grid(column=0, row=2)
+    lsv.grid(column=1, row=2, columnspan=3, sticky="nsew")
+    lfunc.grid(column=0,row=3)
+    lfuncv.grid(column=1,row=3, columnspan=3, sticky="nsew")
+    lfi.grid(column=0,row=4)
+    lfiv.grid(column=1,row=4, columnspan=3, sticky="nsew")
     
     def call_tk_diff():
         ofile = Path(fileString.get()).open(mode="w")
@@ -116,9 +126,9 @@ def main():
         if func:
             print("KeyFunc:",file=ofile)
             print(functext,file=ofile)
-            tk_diff(fromString.get(),toString.get(),ofile,func)
+            tk_diff(fromString.get(),toString.get(),toSort.get(),ofile,func)
         else:
-            tk_diff(fromString.get(),toString.get(),ofile)
+            tk_diff(fromString.get(),toString.get(),toSort.get(),ofile)
 
     b2=ttk.Button(frm,text="Execute",command=call_tk_diff)
     b3=ttk.Button(frm, text="Quit", command=root.destroy)
@@ -129,9 +139,9 @@ def main():
     b4=ttk.Button(frm, text="Swap", command=call_tk_swap)
     
     # b1.grid(column=2, row=2, sticky="nsew")
-    b2.grid(column=1, row=4, sticky="nsew")
-    b3.grid(column=2, row=4, sticky="nsew")
-    b4.grid(column=3, row=4, sticky="nsew")
+    b2.grid(column=1, row=5, sticky="nsew")
+    b3.grid(column=2, row=5, sticky="nsew")
+    b4.grid(column=3, row=5, sticky="nsew")
 
     root.mainloop()
 
@@ -142,8 +152,8 @@ def tk_swap(f0,t0,o0):
     t0.set(f)
     o0.set(o)
     
-def tk_diff(f,t,ofile,func=lambda n:n):
-    do_diff(Path(f),Path(t),ofile,func)
+def tk_diff(f,t,toSort,ofile,func=lambda n:n):
+    do_diff(Path(f),Path(t),toSort,ofile,func)
     
 def pathsToTempfile(topf,topt):
     fpath = Path(topf)
@@ -156,9 +166,8 @@ def pathsToTempfile(topf,topt):
     return tmpfile
 
 def compileFuncObj(text):
-    g = dict()
     l = dict()
-    exec(text,g,l)
+    exec(text,globals(),l)
     if l:
         return list(l.values())[0]
 
@@ -166,4 +175,3 @@ if __name__ == "__main__":
     main()
 
     
-
