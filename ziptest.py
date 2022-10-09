f9df56716ea69110ef057467ce4f71ce24e2d91d
Hirofumi SHINKE <hiro@molloy.local>
2022-10-09 23:53:45 +0900

    *** empty log message ***

--- ziptest.py
+++ ziptest.py
@@ -10,18 +10,64 @@ import tempfile
 
 def main():
 
-    zip = zipfile.ZipFile("sample.zip","w",
-                          compression=zipfile.ZIP_DEFLATED)
+    with zipfile.ZipFile("sample1.zip","w",
+                         compression=zipfile.ZIP_DEFLATED) as zip:
 
-    with zip.open("xxx/a.txt","w") as fh:
-        fh.write(b"xxxxxxxx\n")
-        fh.write(b"xxxxxxxx\n")
+        with zip.open("xxx/a.txt","w") as fh:
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            
+        with zip.open("xxx/yyy/a.txt","w") as fh:
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
 
-    with zip.open("xxx/yyy/a.txt","w") as fh:
-        fh.write(b"xxxxxxxx\n")
-        fh.write(b"xxxxxxxx\n")
+    with zipfile.ZipFile("sample2.zip","w",
+                         compression=zipfile.ZIP_DEFLATED) as zip:
+
+        with zip.open("xxx/a.txt","w") as fh:
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"yyyyyyyy\n")
+            fh.write(b"xxxxxxxx\n")
+            
+        with zip.open("xxx/yyy/a.txt","w") as fh:
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
 
 
+    with zipfile.ZipFile("sample3.zip","w",
+                         compression=zipfile.ZIP_DEFLATED) as zip:
+
+        with zip.open("xxx/a.txt","w") as fh:
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            
+        with zip.open("xxx/yyy/a.txt","w") as fh:
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+
+        zip.write("sample1.zip","xxx/sample.zip")
+
+    with zipfile.ZipFile("sample4.zip","w",
+                         compression=zipfile.ZIP_DEFLATED) as zip:
+
+        with zip.open("xxx/a.txt","w") as fh:
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            
+        with zip.open("xxx/yyy/a.txt","w") as fh:
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+            fh.write(b"xxxxxxxx\n")
+
+        zip.write("sample2.zip","xxx/sample.zip")
+
+            
     
 if __name__ == "__main__":
     main()
