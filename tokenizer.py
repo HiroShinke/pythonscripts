0cc6a10d79092e256eaefd9cfb39820fa4442a8f
Hirofumi SHINKE <hiro@molloy.local>
2022-10-30 21:15:21 +0900

    *** empty log message ***

--- tokenizer.py
+++ tokenizer.py
@@ -14,10 +14,25 @@ def main():
     
     scanner = pat.scanner("abc123")
 
+    print(f"{type(scanner)}")
+    
     while m := scanner.match():
         print(f"{m}")
         print(f"{m.lastgroup}")
         print(f"{m.group()}")
+
+    print("finditer version")
+
+    for m in pat.finditer("abc123"):
+        print(f"{m}")
+        print(f"{m.lastgroup}")
+        print(f"{m.group()}")
+
+    for m in pat.finditer("abc*123"):
+        print(f"{m}")
+        print(f"{m.lastgroup}")
+        print(f"{m.group()}")
+
         
 
 def applyPat(pat,str):
