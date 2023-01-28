7b20960289c24bc8c6ce6cfaf7c096e4ad0e7322
Hirofumi SHINKE <hiro@molloy.local>
2023-01-28 17:50:28 +0900

    srcanalyze for cobol and call tree

--- srcviewer.py
+++ srcviewer.py
@@ -3,6 +3,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.filedialog as fd
+import argparse
 from pathlib import Path
 import re
 
@@ -64,14 +65,87 @@ def syntax_highlight(text,contents):
         start,end = m.span(token_type)
         text.tag_add(token_type,f"1.0 +{start}c",f"1.0 +{end}c")
 
-        
+def cobol_src_analyze(contents):
+
+    lines = contents.splitlines()
+
+    pat = re.compile(r"^.{6}\*")
+
+    line_specification = [
+        ("perform", r"(?<!\w)PERFORM\s+([\w-]+)"),
+        ("division", r"(\S+)\s+DIVISION(?!\w)"),
+        ("section",r"(\S+)\s+SECTION\s*\."),
+        ("call", r'(?<!\w)CALL\s+["\']([^\.]+)["\']')
+    ]
+    line_pat = '|'.join(f'(?P<{p}>{pat})'
+                         for p,pat in line_specification)
+    expre = re.compile(line_pat)
+    inProcedureDivision = False
+
+    currentSec = None
+    perform_dict = {}
+    entrySec = None
+    
+    for l in lines:
+        if pat.search(l):
+            continue
+        if m := expre.search(l):
+            tokentype = m.lastgroup
+            word = m.group(m.lastindex + 1)
+            # print(f"{tokentype},{word}")
+            start,end = m.span(tokentype)
+            if tokentype == "division":
+                if word == "PROCEDURE":
+                    inProcedureDivision = True
+            elif inProcedureDivision and tokentype == "section":
+                currentSec = word
+                if entrySec is None:
+                    entrySec = currentSec
+            elif tokentype == "perform":
+                if ( word != "VARYING" and
+                     word != "UNTIL" and
+                     word != "TIMES" ):
+                    register_multivalue(perform_dict,currentSec,word)
+            elif tokentype == "call":
+                pass
+
+    # print(f"{perform_dict}")
+
+    def recursivePrintSec(p,lev):
+
+        print(" " * lev, p)
+        for c in perform_dict.get(p,[]):
+            recursivePrintSec(c,lev+1)
+
+    recursivePrintSec(entrySec,0)
+
+    return (entrySec,perform_dict)
+    
+            
+def register_multivalue(dict,k,v):
+    e = dict.get(k,None)
+    if e is None:
+        dict[k] = e = [] 
+    e.append(v)
+
 def main():
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--srcdir", "-s")
+    parser.add_argument("--incdir", "-i")    
+    args = parser.parse_args()
+    
+    targetDir = args.srcdir if args.srcdir else "."
+    srcEncoding = "euc-jp"
+    
     root = tk.Tk()
     root.title("simple dir browser")
     
     treeview = TreeView(root)
-    treeview.grid(row=0,column=0,columnspan=2,sticky=tk.N+tk.S+tk.E+tk.W)
+    treeview.grid(row=0,column=0,sticky=tk.N+tk.S+tk.E+tk.W)
+
+    treeview2 = TreeView(root)
+    treeview2.grid(row=0,column=1,sticky=tk.N+tk.S+tk.E+tk.W)
 
     srcview = SrcView(root)
     srcview.grid(row=0,column=2,columnspan=2,sticky=tk.N+tk.S+tk.E+tk.W)
@@ -105,22 +179,62 @@ def main():
             for c in p.iterdir():
                 tree_insert_item(c,item)
 
+    dummy_nodes2 = {}
+    all_nodes2 = {}
+    calldict = {}
+    
+    def tree_insert_item2(p,parent):
+        node = treeview2.tree.insert(parent,tk.END,text=p,open=False)
+        all_nodes2[node] = p            
+        if p in calldict:
+            dummy_nodes2[node] = p            
+            treeview2.tree.insert(node,tk.END)
+
+    def tree_open_item2(e):
+        item = treeview2.tree.focus()
+        p = dummy_nodes2.pop(item,False)
+        if p:
+            children = treeview2.tree.get_children(item)
+            treeview2.tree.delete(children)
+            for c in calldict[p]:
+                tree_insert_item2(c,item)
+
     def tree_select_item(e):
         item = treeview.tree.focus()
         print(f"{item}")
+        item_view_src(item)
+        item_analyze_src(item)
+        
+    def item_view_src(item):
         p = all_nodes[item]
         if p.is_file():
-            with open(p) as fh:
+            with open(p,encoding=srcEncoding) as fh:
                 contents = fh.read()
                 srcview.text.delete("1.0","end -1c")
                 srcview.text.insert("1.0",contents)
                 syntax_highlight(srcview.text,contents)
-                
+        
+    def item_analyze_src(item):
+        p = all_nodes[item]
+        if p.is_file():
+            with open(p,encoding=srcEncoding) as fh:
+                contents = fh.read()
+                entrySec,retdict = cobol_src_analyze(contents)
 
+            dummy_nodes2.clear()
+            all_nodes2.clear()
+            calldict.clear()
+            calldict.update(retdict)
+            if items := treeview2.tree.get_children():
+                treeview2.tree.delete(items)
+            tree_insert_item2(entrySec,"")
+                
+                
     button1 = ttk.Button(root,text="Reload...",command=refresh_tree)
     button1.grid(row=1,column=2,sticky=tk.W+tk.E)
     button2 = ttk.Button(root,text="Quit",command=root.destroy)
     button2.grid(row=1,column=3,sticky=tk.W+tk.E)
+    root.columnconfigure(1,weight=1)
     root.columnconfigure(2,weight=1)
     root.columnconfigure(3,weight=1)
     root.rowconfigure(0,weight=1)
@@ -128,7 +242,9 @@ def main():
     treeview.tree.bind("<Double-1>",event_printer)
     treeview.tree.bind("<<TreeviewOpen>>",tree_open_item)
     treeview.tree.bind("<<TreeviewClose>>",event_printer)
-    treeview.tree.bind("<<TreeviewSelect>>",tree_select_item)    
+    treeview.tree.bind("<<TreeviewSelect>>",tree_select_item)
+
+    treeview2.tree.bind("<<TreeviewOpen>>",tree_open_item2)    
     
     menubar = tk.Menu(root)
     file_menu = tk.Menu(menubar, tearoff=False)
@@ -138,7 +254,7 @@ def main():
     root.config(menu = menubar)
 
     
-    p = Path(".")
+    p = Path(targetDir)
     tree_insert_item(p,"")
             
     root.mainloop()
