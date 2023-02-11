941cc30b1b054d7289e25d87d2d18717c4f74591
Hirofumi SHINKE <hiro@molloy.local>
2023-02-11 10:14:37 +0900

    work for tab action

--- tkfind.py
+++ tkfind.py
@@ -33,7 +33,9 @@ def main():
     typeString.set("")
     
     frm = ttk.Frame(root, padding=10)
-    frm.grid()
+    frm.grid(column=0,row=0,sticky=tk.N+tk.S+tk.E+tk.W)
+    root.columnconfigure(0,weight=1)
+    root.rowconfigure(0,weight=1)
     
     lf=ttk.Label(frm, text="File:")
     lfv = ttk.Entry(frm, textvariable=fromString)
@@ -46,25 +48,62 @@ def main():
     lfuncv = tk.Text(frm, width=80, height=5)
     lpv = ttk.Entry(frm, textvariable=patString)
 
-
-
     lfi=ttk.Label(frm, text="Result:")
     lfiv = ttk.Entry(frm, width=80, textvariable=fileString)
     
     lf.grid(column=0, row=0)
-    lfv.grid(column=1, row=0, columnspan=3, sticky="nsew")
+    lfv.grid(column=1, row=0, columnspan=3, sticky=tk.N+tk.S+tk.E+tk.W)
     lt.grid(column=0, row=1)
-    ltb1.grid(column=1, row=1, sticky="nsew")
-    ltb2.grid(column=2, row=1, sticky="nsew")
-    ltb3.grid(column=3, row=1, sticky="nsew")
+    ltb1.grid(column=1, row=1, sticky=tk.N+tk.S+tk.E+tk.W)
+    ltb2.grid(column=2, row=1, sticky=tk.N+tk.S+tk.E+tk.W)
+    ltb3.grid(column=3, row=1, sticky=tk.N+tk.S+tk.E+tk.W)
     lp.grid(column=0, row=2)
-    lpv.grid(column=1, row=2, columnspan=3, sticky="nsew")
+    lpv.grid(column=1, row=2, columnspan=3, sticky=tk.N+tk.S+tk.E+tk.W)
     lfunc.grid(column=0,row=3)
-    lfuncv.grid(column=1,row=3, columnspan=3, sticky="nsew")
+    lfuncv.grid(column=1,row=3, columnspan=3, sticky=tk.N+tk.S+tk.E+tk.W)
     lfi.grid(column=0,row=4)
-    lfiv.grid(column=1,row=4, columnspan=3, sticky="nsew")
-    
+    lfiv.grid(column=1,row=4, columnspan=3, sticky=tk.N+tk.S+tk.E+tk.W)
 
+    def tab_event(e):
+        print(f"event = {e}")
+        print(f"state = {e.state}")
+
+        prev_height = -4        
+        num_goback = 1
+        while True:
+            idx = lfuncv.index(f"insert -{num_goback}l linestart")
+            prevline = lfuncv.get(f"insert -{num_goback}l linestart",f"insert -{num_goback}l lineend")
+            if m := re.search(r"\S",prevline):
+                prev_height,_ = m.span()
+                break
+            elif idx = "1.0"
+                break
+            num_goback += 1
+        
+        line = lfuncv.get("insert linestart","insert lineend")
+        if m := re.search(r"^\s*",line):
+            _,s = m.span()
+        else:
+            s = 0
+
+        pos = lfuncv.index("insert")
+        if m := re.search(r"(\d+)\.(\d+)",pos):
+            l,c= map(lambda x: int(x), m.groups())
+
+        if c < s:
+            lfuncv.mark_set("insert",f"insert linestart +{s}c")
+        else:
+            r = int(s)%4
+            if s < prev_height + 4:
+                lfuncv.insert("insert linestart"," "*(4-r))
+            else:
+                lfuncv.delete("insert linestart",f"insert linestart +{s}c")
+
+        return "break"
+
+    
+    lfuncv.bind("<Tab>",tab_event)
+    
     def call_do_grep():
 
         if patStr:= patString.get():
@@ -84,7 +123,8 @@ def main():
             def pred(f):
                 return f.is_dir()
 
-        of = Path(fileString.get()).open(mode='w')
+        # of = Path(fileString.get()).open(mode='w')
+        of = sys.stdout
         top = Path(fromString.get())
 
         functext = lfuncv.get("1.0","end -1c")            
@@ -93,22 +133,26 @@ def main():
             print(functext,file=of)
             func = compileFuncObj(functext)
         else:
-            def func(f,of):
+            def func(f):
                 relative = f.relative_to(top.parent)
-                print(f"{relative}",file=of)
+                print(f"{relative}")
             
         def grep_proc(f):
             if pred(f) and ( pat is None or pat.search(f.name) ):
-                func(f,of)
+                func(f)
             
         do_rec_file(top,grep_proc)
             
     b2=ttk.Button(frm,text="Execute",command=call_do_grep)
     b3=ttk.Button(frm, text="Quit", command=root.destroy)
 
-    # b1.grid(column=2, row=2, sticky="nsew")
-    b2.grid(column=2, row=4, sticky="nsew")
-    b3.grid(column=3, row=4, sticky="nsew")
+    # b1.grid(column=2, row=2, sticky=tk.N+tk.S+tk.E+tk.W)
+    b2.grid(column=2, row=4, sticky=tk.N+tk.S+tk.E+tk.W)
+    b3.grid(column=3, row=4, sticky=tk.N+tk.S+tk.E+tk.W)
+    frm.columnconfigure(1,weight=1)
+    frm.columnconfigure(2,weight=1)
+    frm.columnconfigure(3,weight=1)
+    frm.rowconfigure(3,weight=1)
 
     root.mainloop()
 
