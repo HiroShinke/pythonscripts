c1c22302a7b4d6cd26cb602d77668202022d15ae
Hirofumi SHINKE <hiro@molloy.local>
2023-02-19 12:34:24 +0900

    support pred, pat, ...

--- tkfind2work.py
+++ tkfind2work.py
@@ -5,28 +5,23 @@ import os
 import multiprocessing as mp
 import concurrent.futures
 import queue
-
-def func(f):
-    print(f"{f}")
-
-def do_rec_file(fp,queue):
-
-    try:
-        func(fp)
-        if fp.is_dir():
-            for c in fp.iterdir():
-                queue.put(c)
-    except Exception as e:
-        print(f"{fp} {e}")
+import re
 
 
-def call_do_grep(top,patStr,type,functext=None,callback=None):
-                
-    if patStr:
-        pat = re.compile(patStr)
+def func(f,pat):
+    if f.is_file() and pat:
+        with open(f) as fh:
+            contents = fh.read()
+            lines = contents.splitlines()
+            for l in lines:
+                if pat.search(l):
+                    print(f"{f}: {l}")
     else:
-        pat = None
-    
+        print(f"{f}")
+
+        
+def do_rec_file(fp,type,pat,queue):
+
     if not type:
         def pred(f):
             return True
@@ -36,7 +31,23 @@ def call_do_grep(top,patStr,type,functext=None,callback=None):
     elif type == "d":
         def pred(f):
             return f.is_dir()
-        
+    try:
+        if pred(fp):
+            func(fp,pat)
+        if fp.is_dir():
+            for c in fp.iterdir():
+                queue.put(c)
+    except Exception as e:
+        print(f"{fp} {e}")
+
+
+def call_do_grep(top,patStr,type,functext=None,callback=None):
+
+    if patStr:
+        pat = re.compile(patStr)
+    else:
+        pat = None
+    
     of = sys.stdout
     que = mp.Manager().Queue()
     que.put(top)
@@ -59,7 +70,7 @@ def call_do_grep(top,patStr,type,functext=None,callback=None):
             try:                
                 while fp := que.get(False):
                     count_processing += 1
-                    fut = executor.submit(do_rec_file,fp,que)
+                    fut = executor.submit(do_rec_file,fp,type,pat,que)
                     futures.add(fut)
                     if callback(count_processing,count_done):
                         print(f"executor.shutdown() future count = {len(futures)}")
