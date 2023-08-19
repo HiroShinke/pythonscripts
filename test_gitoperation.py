6f298d4609a1aa3b1e3b327916186582d5acfaa1
Your Name <you@example.com>
2023-08-19 16:13:13 +0900

    test_merge2

--- test_gitoperation.py
+++ test_gitoperation.py
@@ -392,6 +392,136 @@ goodby africa
 """
                          ,ret)
 
+    def test_merge2(self):
+
+        os.environ["GIT_AUTHOR_DATE"] = "Fri May 5 20:30:55 2023 +0900"
+        os.environ["GIT_COMMITTER_DATE"] = "Fri May 5 20:30:55 2023 +0900"
+        
+        make_file("hello.txt","""\
+hello, world
+"""
+                  )
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m A')
+
+        make_file("hello.txt","""\
+hello, world
+goodby japan
+"""
+                  )
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m B')
+
+        make_file("hello.txt","""\
+hello, world
+goodby japan
+goodby america
+"""
+                  )
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m C')
+
+        ret = cmd_stdout("git log --graph --abbrev-commit --oneline")
+        self.assertEqual("""\
+* 40c9072 C
+* b3db10b B
+* 70809e4 A
+"""
+                         ,ret)
+
+        cmd_stdout("git checkout -b new-topic")
+
+        make_file("hello.txt","""\
+hello, world
+goodby japan
+goodby america
+goodby europe
+"""
+                  )
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m D')
+
+        ret = cmd_stdout("git log --graph --abbrev-commit --oneline")
+        self.assertEqual("""\
+* 36da5b2 D
+* 40c9072 C
+* b3db10b B
+* 70809e4 A
+"""
+                         ,ret)
+        
+        cmd_stdout("git checkout master")
+
+        make_file("hello.txt","""\
+hello, world
+goodby japan
+goodby america
+goodby africa
+"""
+                  )
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m F')
+
+        ret = cmd_stdout("git log --graph --abbrev-commit --oneline")
+        self.assertEqual("""\
+* 65baea3 F
+* 40c9072 C
+* b3db10b B
+* 70809e4 A
+"""
+                         ,ret)
+
+        with self.assertRaises(Exception) as cm:
+            cmd_stdout("git merge new-topic")
+
+        self.assertEqual("""\
+hello, world
+goodby japan
+goodby america
+<<<<<<< HEAD
+goodby africa
+=======
+goodby europe
+>>>>>>> new-topic
+"""
+                         ,cmd_stdout("cat hello.txt"))
+            
+        make_file("hello.txt","""\
+hello, world
+goodby japan
+goodby america
+goodby europe
+goodby africa
+"""
+                  )
+
+
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m "F\'"')
+        
+        ret = cmd_stdout("git log --graph --abbrev-commit --oneline")
+        self.assertEqual("""\
+*   6b8756b F'
+|\  
+| * 36da5b2 D
+* | 65baea3 F
+|/  
+* 40c9072 C
+* b3db10b B
+* 70809e4 A
+"""
+                         ,ret)
+
+        ret = cmd_stdout("git show-branch")
+        self.assertEqual("""\
+* [master] F'
+ ! [new-topic] D
+--
+-  [master] F'
+*+ [new-topic] D
+"""
+                         ,ret)
+
     def test_rebase1(self):
 
         os.environ["GIT_AUTHOR_DATE"] = "Fri May 5 20:30:55 2023 +0900"
